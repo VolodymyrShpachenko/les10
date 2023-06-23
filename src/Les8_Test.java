0febdcb2e88355721c4395310570d38aa471b57c
Vladimir Shpachenko <kniyzvladimir@gmail.com>
2023-06-23 17:02:27 +0300

    ex1 done

---
+++ src/Les8_Test.java
@@ -0,0 +1,7 @@
+import ex1.PhoneNumberValidation;
+
+public class Les8_Test {
+    public static void main(String[] args) {
+        new PhoneNumberValidation().validation();
+    }
+}
--- src/ex1/PhoneNumberValidation.java
+++ src/ex1/PhoneNumberValidation.java
@@ -4,22 +4,27 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PhoneNumberValidation {
     public void validation() {
+        Pattern pattern1 = Pattern.compile("\\(\\d{3}\\) \\d{3}-\\d{4}");
+        Pattern pattern2 = Pattern.compile("\\d{3}-\\d{3}-\\d{4}");
         try (BufferedReader reader = new BufferedReader(new FileReader("text.txt"))) {
             String line;
             while ((line=reader.readLine()) != null ) {
-                System.out.println(line);
+                Matcher matcher1 = pattern1.matcher(line);
+                Matcher matcher2 = pattern2.matcher(line);
+                if (matcher1.find() || matcher2.find()) {
+                    System.out.println(line);
+                }
             }
+
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    public static void main(String[] args) {
-        new PhoneNumberValidation().validation();
     }
 }
